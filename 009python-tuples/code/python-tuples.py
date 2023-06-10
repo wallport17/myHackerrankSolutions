3e37d68207c7e79169330b061b9358d4f4e9877b
vim <allportwc@appstate>
2023-06-09 21:17:11 -0400

    Second commit for tuples 	modified:   009python-tuples/code/python-tuples.py

--- 009python-tuples/code/python-tuples.py
+++ 009python-tuples/code/python-tuples.py
@@ -1,7 +1,7 @@
 f __name__ == '__main__':
-    n = int(input())
-    integer_list = map(int, input().split())
+    n = int(input()) #Here the variable is declared. It asks for an input that will be an intger
+    integer_list = map(int, input().split()) #Here we are transforming our input and spliting the string into substrings
                 
-    t = tuple(integer_list)
-    print(hash(t))
+    t = tuple(integer_list) #Here we are creating a tuple from the integer list
+    print(hash(t)) #
                              

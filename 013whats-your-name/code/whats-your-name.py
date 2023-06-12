af457341f5082c0a60336976c1e613a25430c91e
vim <allportwc@appstate>
2023-06-12 14:38:41 -0400

    First commit what's your name 	new file:   013whats-your-name/code/whats-your-name.py

---
+++ 013whats-your-name/code/whats-your-name.py
@@ -0,0 +1,9 @@
+
+def print_ful_name(first, last):
+    output = "Hello " + first + " " + last + "! You just delved into python."
+    print(output)
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)

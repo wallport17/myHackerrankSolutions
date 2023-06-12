bae89f0e7ef5d44a011c962c5e3d10b158fe5b46
vim <allportwc@appstate>
2023-06-12 14:31:26 -0400

    Second commit for finding percentage 	modified:   012finding-the-percentage/code/finding-the-percentage.py

--- 012finding-the-percentage/code/finding-the-percentage.py
+++ 012finding-the-percentage/code/finding-the-percentage.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
         scores = list(map(float, line))
         student_marks[name] = scores
      query_name = input()
-     read = student_marks[query_name]
-     sum = sum(read) / len(read)
-     print("{:.2f}".format(sum))
+     read = student_marks[query_name] #This line gets the scores of a particular student in the student_marks dictionary
+     sum = sum(read) / len(read) #This line takes the sum of 'read' and divides it by the length of read 
+                                 #to get the average
+     print("{:.2f}".format(sum)) 

8a095bc3b6ec3bec8373e7d86c651880cf06db8d
vim <allportwc@appstate>
2023-06-12 13:51:07 -0400

    Third commit for list comprehensions 	modified:   010list-comprehensions/code/list-comprehensions.py

--- 010list-comprehensions/code/list-comprehensions.py
+++ 010list-comprehensions/code/list-comprehensions.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     #Here we are given input that represents the integer the elements of the array can not equal
     n = int(input())
     def get_coordinates(x, y, z):
+        #This line will create the coordinates [1, j, k] by iterating i from 0 to x, y, or z. It then takes the sum and checks to see if it the sum is not equal to n. If it isn't it will be added to the list of coordinates.
         coordinates = [[i, j, k] for i  in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
         return coordinates
     print(get_coordinates(x, y, z))

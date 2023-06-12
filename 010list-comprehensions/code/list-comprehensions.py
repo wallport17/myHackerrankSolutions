05b6345ca5df85e1a220e613033b795a3eeddf60
vim <allportwc@appstate>
2023-06-12 13:38:52 -0400

    Second commit for list comprehensions 	modified:   010list-comprehensions/code/list-comprehensions.py

--- 010list-comprehensions/code/list-comprehensions.py
+++ 010list-comprehensions/code/list-comprehensions.py
@@ -1,7 +1,9 @@
 if __name__ == '__main__':
+    #Here we take input from user and get the integers representing the dimensions of a cubiod
     x = int(input())
     y = int(input())
     z = int(input())
+    #Here we are given input that represents the integer the elements of the array can not equal
     n = int(input())
     def get_coordinates(x, y, z):
         coordinates = [[i, j, k] for i  in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]

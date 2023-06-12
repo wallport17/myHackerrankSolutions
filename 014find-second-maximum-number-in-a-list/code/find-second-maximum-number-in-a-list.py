a558daa1c22001e2090100f93b1f881a00e02e94
vim <allportwc@appstate>
2023-06-12 15:06:15 -0400

    Second commit max number 	modified:   014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py

--- 014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py
+++ 014find-second-maximum-number-in-a-list/code/find-second-maximum-number-in-a-list.py
@@ -1,9 +1,9 @@
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-x = list(arr)
-x.sort()
-max = (max(x))
-while max in x:
+x = list(arr) #Here I create a list from arr
+x.sort() #Here I sort that list from smallest to largest
+max = (max(x)) #Here a create a variable to hold the max value of the list
+while max in x: #Here I create a check that removes all value equal to the max
     x.remove(max)
 print(x[-1])

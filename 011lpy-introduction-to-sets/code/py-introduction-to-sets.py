5f84c545bfa747f36c3dd7fab36266f530fc894c
vim <allportwc@appstate>
2023-06-12 14:09:10 -0400

    Second commit for intro to sets 	modified:   011lpy-introduction-to-sets/code/py-introduction-to-sets.py

--- 011lpy-introduction-to-sets/code/py-introduction-to-sets.py
+++ 011lpy-introduction-to-sets/code/py-introduction-to-sets.py
@@ -1,6 +1,6 @@
-def average(array):
-    ray = set(array)
-    average = sum(ray) / len(ray)
+def average(array): #Here we define the function average to take array as input
+    ray = set(array) #Here we create a set from the input
+    average = sum(ray) / len(ray) #Here we calculate average by taking the sum of the set and dividing it by the length
     return average
 
 

864b928f6daace6f828aa01cc0fde7432d524da2
William <allportwc@appstate.edu>
2023-06-14 18:22:59 -0400

    First commit for np array 	new file:   015np-arrays/code/np-arrays.py

---
+++ 015np-arrays/code/np-arrays.py
@@ -0,0 +1,12 @@
+import numpy #Here we import the NumPy library
+
+def arrays(arr): #Here we define arrays with arr as the parameter
+    
+    y = numpy.array(arr,float) #Here we created a NumPy array with arr as
+                               #input and float as the element type 
+    z = y[::-1]
+    return z
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)

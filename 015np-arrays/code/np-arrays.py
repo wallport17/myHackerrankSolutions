e3b80fad2bb6b55bb5ac14f22a65a2eb413461bc
William <allportwc@appstate.edu>
2023-06-14 18:24:29 -0400

    Second commit for np array 	modified:   015np-arrays/code/np-arrays.py

--- 015np-arrays/code/np-arrays.py
+++ 015np-arrays/code/np-arrays.py
@@ -4,7 +4,7 @@ def arrays(arr): #Here we define arrays with arr as the parameter
     
     y = numpy.array(arr,float) #Here we created a NumPy array with arr as
                                #input and float as the element type 
-    z = y[::-1]
+    z = y[::-1] #Here we reversed the np array
     return z
 
 arr = input().strip().split(' ')

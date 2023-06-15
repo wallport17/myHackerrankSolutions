c22868e66be69c27257cbe03a326fb80d1604c2c
William <allportwc@appstate.edu>
2023-06-14 20:28:29 -0400

    Third commit swap case 	modified:   016swap-case/code/swap-case.py

--- 016swap-case/code/swap-case.py
+++ 016swap-case/code/swap-case.py
@@ -1,7 +1,9 @@
 def swap_case(s): #Here the function swap_case is defined with s as the
                   #Parameter
-    swapped = s.swapcase() 
-    return swapped
+    swapped = s.swapcase() #Here the variable swapped is declared.
+                           #.swapcase is a method that switches 
+                           # uppercase w/ lowercase and calls on s 
+    return swapped #Now swapped is returned to the defined function 
 
 if __name__ == '__main__':
     s = input()

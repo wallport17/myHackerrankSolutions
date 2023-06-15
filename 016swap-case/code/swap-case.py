7da2e11f36d9d315b30bc298c440a63bc430451d
William <allportwc@appstate.edu>
2023-06-14 20:22:32 -0400

    Second commit swap case 	modified:   016swap-case/code/swap-case.py

--- 016swap-case/code/swap-case.py
+++ 016swap-case/code/swap-case.py
@@ -1,5 +1,6 @@
-def swap_case(s):
-    swapped = s.swapcase()
+def swap_case(s): #Here the function swap_case is defined with s as the
+                  #Parameter
+    swapped = s.swapcase() 
     return swapped
 
 if __name__ == '__main__':

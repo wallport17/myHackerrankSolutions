cc28264e3142da6d66af82fab394188bb46dde32
William <allportwc@appstate.edu>
2023-06-16 11:08:54 -0400

    Third commit for split and join 	modified:   017python-string-split-and-join/code/python-string-split-and-join.py

--- 017python-string-split-and-join/code/python-string-split-and-join.py
+++ 017python-string-split-and-join/code/python-string-split-and-join.py
@@ -2,8 +2,9 @@
 def split_and_join(line):
     a = line.split(" ") #Here i split line at the spaces and converted it 
                         #into a list of strings
-    b = "-".join(a)
-    return b
+    b = "-".join(a) #Here I declared the variable b which will join the list
+                    #of strings with a "-"
+    return b #Here I return my 'joined' list
 
 if __name__ == '__main__':
     line = input()
